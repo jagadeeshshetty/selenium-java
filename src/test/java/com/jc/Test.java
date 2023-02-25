b030a59ff10e780959c4a4773ac2b6567d3d4afa
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2023-02-26 00:49:04 +0530

    update

--- src/test/java/com/jc/Test.java
+++ src/test/java/com/jc/Test.java
@@ -2,6 +2,6 @@ package com.jc;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Running Test class.");
+        System.out.println("Running main method within the " + Test.class.getSimpleName() + " class.");
     }
 }
