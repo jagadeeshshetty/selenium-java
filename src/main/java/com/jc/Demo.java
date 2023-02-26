b6eb6ec4feeedd40bd8bc78df62bcdb356c5fd62
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2023-02-26 19:51:19 +0530

    strings update

--- src/main/java/com/jc/Demo.java
+++ src/main/java/com/jc/Demo.java
@@ -4,7 +4,8 @@ public class Demo {
   public static void main(String[] args) {
     System.out.println('a'); // character string
     System.out.println("Steve Jobs"); // Words as a string
-
     System.out.println(2 + 2 + '2'); // 54 - 2+2 is 4. String '2' ASCII is 50.
+
+    System.out.println(2 + 2);
   }
 }
--- src/main/java/com/jc/Test.java
+++ src/main/java/com/jc/Test.java
@@ -2,16 +2,7 @@ package com.jc;
 
 public class Test {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 10;
-        int c = 15;
-        if (a == 5) {
-            System.out.println(b + c); // 25
-            System.out.println("Hi"); // 25
-        }
-
-        if (a != 5) {
-            System.out.println(b - c); // 5
-        }
+        System.out.println(Test.class.getSimpleName());
     }
+
 }
