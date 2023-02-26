fb724f276c51a6cc4718c84f40372470b14087b0
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2023-02-26 14:26:03 +0530

    added string readme file.

---
+++ src/main/java/com/jc/Test.java
@@ -0,0 +1,7 @@
+package com.jc;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Running main method within the " + Test.class.getSimpleName() + " class.");
+    }
+}
